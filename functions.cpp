bbd272be333a8659d83254198bdfb243aea10ade
Chris <cbzoozoom7@gmail.com>
2023-01-26 16:28:42 -0600

    wrote int string_length()

--- functions.cpp
+++ functions.cpp
@@ -4,6 +4,9 @@ Chris
 Created 26 Jan 2023
 */
 #include "functions.h"
-int string_length(const char *str) {
-    return 0;
+int string_length(const char* str) {
+    int i = 0;
+    while (str[i] != '\0') //all strings terminate with '\0'
+        i++;
+    return i;
 }
