e615c38bad281a3ae885c451ed062156bf086b7c
Chris <cbzoozoom7@gmail.com>
2023-01-26 16:58:33 -0600

    added brackets to that single line while because the lecture warned me.

--- functions.cpp
+++ functions.cpp
@@ -6,7 +6,8 @@ Created 26 Jan 2023
 #include "functions.h"
 int string_length(const char* str) {
     int i = 0;
-    while (str[i] != '\0') //all strings terminate with '\0'
+    while (str[i] != '\0') { //all strings terminate with '\0'
         i++;
+    }
     return i;
 }
